a8b632f755fb9f0b880e55337a1ad6016f854542
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-10 11:44:06 +0530

    Setting up SPRING MVC cofiguration done. 🤑

---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String home() {
+		return "home";  // This is the name of the view file
+	}
+}
